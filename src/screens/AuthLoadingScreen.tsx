0389ef924c4cf33fc3aacada2c02cc3e90d114e1
Rui Ying <yingrui205@gmail.com>
2019-04-11 16:44:28 +0800

    Optimize auth check

--- src/screens/AuthLoadingScreen.tsx
+++ src/screens/AuthLoadingScreen.tsx
@@ -4,41 +4,56 @@ import SplashScreen from "../components/SplashScreen";
 import { login } from "../redux/actions/auth";
 import { getCurrentSemester } from "../redux/actions/currentSemester";
 import { getAllSemesters } from "../redux/actions/semesters";
-import { IAuth, IPersistAppState, ISemester } from "../redux/types/state";
+import { IAuthState, IPersistAppState, ISemester } from "../redux/types/state";
 import { INavigationScreen } from "../types/NavigationScreen";
 
-type IAuthLoadingScreenProps = DispatchProp<any> & {
+interface IAuthLoadingScreenStateProps {
   readonly rehydrated: boolean;
-  readonly auth: IAuth;
+  readonly auth: IAuthState;
   readonly semesters: ReadonlyArray<ISemester>;
-};
+}
+
+type IAuthLoadingScreenProps = DispatchProp<any> & IAuthLoadingScreenStateProps;
 
 const AuthLoadingScreen: INavigationScreen<IAuthLoadingScreenProps> = props => {
   const { navigation, dispatch, rehydrated, auth, semesters } = props;
 
   useEffect(() => {
+    if (auth.loggedIn) {
+      dispatch(getAllSemesters());
+      dispatch(getCurrentSemester(semesters));
+      navigation.navigate("Main");
+    }
+  }, [auth.loggedIn]);
+
+  useEffect(() => {
+    if (auth.error) {
+      navigation.navigate("Auth");
+    }
+  }, [auth.error]);
+
+  useEffect(() => {
     if (rehydrated) {
-      (async () => {
-        if (auth && auth.username && auth.password) {
-          await Promise.resolve(dispatch(login(auth.username, auth.password)));
-          dispatch(getAllSemesters());
-          dispatch(getCurrentSemester(semesters));
-          navigation.navigate("Main");
-        } else {
-          navigation.navigate("Auth");
-        }
-      })();
+      if (auth && auth.username && auth.password) {
+        dispatch(login(auth.username, auth.password));
+      } else {
+        navigation.navigate("Auth");
+      }
     }
   }, [rehydrated]);
 
   return <SplashScreen />;
 };
 
-const mapStateToProps = (state: IPersistAppState) => ({
-  rehydrated: state.auth._persist.rehydrated,
-  auth: { username: state.auth.username, password: state.auth.password },
-  semesters: state.semesters.items
-});
+function mapStateToProps(
+  state: IPersistAppState
+): IAuthLoadingScreenStateProps {
+  return {
+    rehydrated: state.auth._persist.rehydrated,
+    auth: state.auth,
+    semesters: state.semesters.items
+  };
+}
 
 export default connect(
   mapStateToProps,
