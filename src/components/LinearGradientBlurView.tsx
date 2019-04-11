0baf1b92f0aa9b046ba2399a1293dbe7b8d8d0c8
Robert Ying <yingrui205@gmail.com>
2019-04-11 16:46:29 +0800

    Reduce `blurAmount`

--- src/components/LinearGradientBlurView.tsx
+++ src/components/LinearGradientBlurView.tsx
@@ -15,7 +15,7 @@ const LinearGradientBlurView: React.FunctionComponent<
       props.style
     ]}
   >
-    <BlurView blurAmount={50} blurType="dark" style={styles.absolute} />
+    <BlurView blurAmount={25} blurType="dark" style={styles.absolute} />
     <LinearGradient
       colors={Colors.headerGradient}
       style={styles.absolute}
