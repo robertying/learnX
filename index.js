817cab452a91f10d8ae569df4c7e32e5bf868836
Robert Ying <yingrui205@gmail.com>
2019-04-11 12:27:09 +0800

    Fix animation on Android

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-import { AppRegistry } from "react-native";
+import { AppRegistry, Platform, UIManager } from "react-native";
 import { name as appName } from "./app.json";
 import App from "./src/App";
 
 console.disableYellowBox = true;
+
+if (Platform.OS === "android") {
+  UIManager.setLayoutAnimationEnabledExperimental &&
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 AppRegistry.registerComponent(appName, () => App);
