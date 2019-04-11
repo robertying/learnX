66d8e43499fbd0aa203a0e67cf621ad08c0e558a
Robert Ying <yingrui205@gmail.com>
2019-04-11 17:57:25 +0800

    Add loading placeholder for WebView (#10)

--- src/screens/WebViewScreen.tsx
+++ src/screens/WebViewScreen.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { View } from "react-native";
 import { WebView } from "react-native-webview";
+import NoticePlaceholder from "../components/NoticePlaceholder";
 import { INavigationScreen } from "../types/NavigationScreen";
 
 export interface IWebViewScreenProps {
@@ -9,13 +11,36 @@ export interface IWebViewScreenProps {
 const WebViewScreen: INavigationScreen<IWebViewScreenProps> = props => {
   const url = props.navigation.getParam("url");
 
+  const [loading, setLoading] = useState(true);
+
   return (
-    <WebView
-      source={{
-        uri: url
-      }}
-      useWebKit={false}
-    />
+    <>
+      <WebView
+        source={{
+          uri: url
+        }}
+        useWebKit={false}
+        // tslint:disable-next-line: jsx-no-lambda
+        onLoadEnd={() => setLoading(false)}
+      />
+      {loading && (
+        <View
+          style={{
+            backgroundColor: "white",
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0
+          }}
+        >
+          <NoticePlaceholder style={{ margin: 15 }} loading={true} />
+          <NoticePlaceholder style={{ margin: 15 }} loading={true} />
+          <NoticePlaceholder style={{ margin: 15 }} loading={true} />
+          <NoticePlaceholder style={{ margin: 15 }} loading={true} />
+        </View>
+      )}
+    </>
   );
 };
 
