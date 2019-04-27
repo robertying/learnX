7d7ee97545203ecce014b6c402a36aa3a677a773
Robert Ying <yingrui205@gmail.com>
2019-04-27 11:21:54 +0800

    Update NavigationScreenProp type def

--- src/types/NavigationScreen.ts
+++ src/types/NavigationScreen.ts
@@ -1,11 +1,16 @@
 import {
+  NavigationParams,
+  NavigationRoute,
   NavigationScreenConfig,
   NavigationScreenOptions,
   NavigationScreenProp
 } from "react-navigation";
 
 export interface INavigationScreenProps {
-  readonly navigation: NavigationScreenProp<{}>;
+  readonly navigation: NavigationScreenProp<
+    NavigationRoute<NavigationParams>,
+    NavigationParams
+  >;
 }
 
 export type INavigationScreen<P> = React.FunctionComponent<
