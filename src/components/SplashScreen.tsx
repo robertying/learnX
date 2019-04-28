64fe62aab8a4e91058182398481621619db2a427
Robert Ying <yingrui205@gmail.com>
2019-04-28 12:11:19 +0800

    Fix tab bar height on iOS other than iPhone X

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,32 +1,31 @@
 import React from "react";
-import { Platform, View } from "react-native";
+import { Platform, SafeAreaView, View } from "react-native";
 import Layout from "../constants/Layout";
 import LinearGradientBlurView from "./LinearGradientBlurView";
 
 const SplashScreen = () => (
-  <View
-    style={{
-      backgroundColor: "#f0f0f0",
-      flex: 1
-    }}
-  >
+  <>
     <LinearGradientBlurView
       style={{
         flex: undefined,
         height: Layout.statusBarHeight + (Platform.OS === "android" ? 56 : 44)
       }}
     />
-    <View
+    <SafeAreaView
       style={{
-        position: "absolute",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        height: Platform.OS === "android" ? 50 : 82,
-        backgroundColor: "white"
+        backgroundColor: "white",
+        flex: 1
       }}
-    />
-  </View>
+    >
+      <View style={{ backgroundColor: "#f0f0f0", flex: 1 }} />
+      <View
+        style={{
+          height: Platform.OS === "android" ? 50 : 49,
+          backgroundColor: "white"
+        }}
+      />
+    </SafeAreaView>
+  </>
 );
 
 export default SplashScreen;
