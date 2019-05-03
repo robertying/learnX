a220f9e265f2074900772254c378252f01ced55c
Rui Ying <yingrui205@gmail.com>
2019-05-03 23:17:00 +0800

    Update project

--- scripts/pack_secrets.sh
+++ scripts/pack_secrets.sh
@@ -3,5 +3,5 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR/../
 
-tar cvf secrets.tar.gz android/keystores/release.keystore .env.default
+tar cvf secrets.tar.gz android/keystores/release.keystore fastlane/key.json
 travis encrypt-file secrets.tar.gz --add
