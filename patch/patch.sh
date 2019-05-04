8b7167f52a1fbb4382c835126269d897c670621b
Robert Ying <yingrui205@gmail.com>
2019-05-04 13:14:26 +0800

    Fix `react-native-interactable` soft link

--- patch/patch.sh
+++ patch/patch.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 cp patch/redux-persist-expo-securestore.js node_modules/redux-persist-expo-securestore/src/index.js
+
+pushd node_modules/react-native-interactable
+rm -rf \{ios\,android\} && rm -rf android && rm -rf ios
+ln -s lib/android . && ln -s lib/ios .
+popd
