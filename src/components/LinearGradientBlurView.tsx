e69d88468c33ca517ff293f2379ff40ef77133ee
Rui Ying <yingrui205@gmail.com>
2019-05-10 10:54:43 +0800

    Revert b844bf2c941b0ac3b0561154534eea178c4b20e0

--- src/components/LinearGradientBlurView.tsx
+++ src/components/LinearGradientBlurView.tsx
@@ -1,6 +1,6 @@
 import { BlurView } from "@react-native-community/blur";
-import React, { useRef } from "react";
-import { findNodeHandle, StyleSheet, View, ViewProps } from "react-native";
+import React from "react";
+import { StyleSheet, View, ViewProps } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import Colors from "../constants/Colors";
 
@@ -8,36 +8,24 @@ export type ILinearGradientBlurViewProps = ViewProps;
 
 const LinearGradientBlurView: React.FunctionComponent<
   ILinearGradientBlurViewProps
-> = props => {
-  const viewRef = useRef<LinearGradient>(null);
-
-  return (
-    <View
-      style={[
-        { flex: 1, justifyContent: "center", alignItems: "center" },
-        props.style
-      ]}
+> = props => (
+  <View
+    style={[
+      { flex: 1, justifyContent: "center", alignItems: "center" },
+      props.style
+    ]}
+  >
+    <BlurView blurAmount={25} blurType="dark" style={styles.absolute} />
+    <LinearGradient
+      colors={Colors.headerGradient}
+      style={styles.absolute}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
     >
-      {findNodeHandle(viewRef.current) && (
-        <BlurView
-          blurAmount={25}
-          blurType="dark"
-          style={styles.absolute}
-          viewRef={findNodeHandle(viewRef.current)}
-        />
-      )}
-      <LinearGradient
-        ref={viewRef}
-        colors={Colors.headerGradient}
-        style={styles.absolute}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-      >
-        {props.children}
-      </LinearGradient>
-    </View>
-  );
-};
+      {props.children}
+    </LinearGradient>
+  </View>
+);
 
 export default LinearGradientBlurView;
 
