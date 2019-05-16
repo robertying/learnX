05657f49a4874b10ebbae5311e198d81d175ce4f
Robert Ying <yingrui205@gmail.com>
2019-05-17 01:00:27 +0800

    Move secret keys to individual files

--- scripts/pack_secrets.sh
+++ scripts/pack_secrets.sh
@@ -3,5 +3,7 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR/../
 
-tar cvf secrets.tar.gz android/keystores/release.keystore fastlane/key.json
+tar cvf secrets.tar.gz android/keystores/release.keystore fastlane/key.json \
+    ios/GoogleService-Info.plist ios/CodePush.plist \
+    android/app/google-services.json android/app/src/main/res/values/codepush.xml
 travis encrypt-file secrets.tar.gz --add
