b4aa0e6c74534223eaa4a9bf4dc4a44a535b865a
Rui Ying <yingrui205@gmail.com>
2019-05-24 23:11:56 +0800

    Add current semester selection

--- src/redux/actions/currentSemester.ts
+++ src/redux/actions/currentSemester.ts
@@ -1,7 +1,11 @@
 import { createAction } from "typesafe-actions";
-import { GET_CURRENT_SEMESTER } from "../types/constants";
+import { GET_CURRENT_SEMESTER, SET_CURRENT_SEMESTER } from "../types/constants";
 import { ISemester } from "../types/state";
 
 export const getCurrentSemester = createAction(GET_CURRENT_SEMESTER, action => {
   return (semesters: ReadonlyArray<ISemester>) => action(semesters);
+});
+
+export const setCurrentSemester = createAction(SET_CURRENT_SEMESTER, action => {
+  return (semester: ISemester) => action(semester);
 });
--- src/redux/reducers/currentSemester.ts
+++ src/redux/reducers/currentSemester.ts
@@ -1,18 +1,23 @@
-import { IGetCurrentSemesterAction } from "../types/actions";
-import { GET_CURRENT_SEMESTER } from "../types/constants";
+import {
+  IGetCurrentSemesterAction,
+  ISetCurrentSemesterAction
+} from "../types/actions";
+import { GET_CURRENT_SEMESTER, SET_CURRENT_SEMESTER } from "../types/constants";
 import { ISemester } from "../types/state";
 
 export default function currentSemester(
   state: ISemester = "2018-2019-2",
-  action: IGetCurrentSemesterAction
+  action: IGetCurrentSemesterAction | ISetCurrentSemesterAction
 ): ISemester {
   switch (action.type) {
     case GET_CURRENT_SEMESTER:
       if (action.payload && action.payload.length !== 0) {
-        return action.payload[0];
+        return state || action.payload[0];
       } else {
         return state;
       }
+    case SET_CURRENT_SEMESTER:
+      return action.payload;
   }
   return state;
 }
