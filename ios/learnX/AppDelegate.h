3530dea9e6366c79d42f919bbcec9d9ee76551a5
Robert Ying <yingrui205@gmail.com>
2019-06-20 09:29:19 +0800

    Upgrade to react-native 0.60.0

--- ios/learnX/AppDelegate.h
+++ ios/learnX/AppDelegate.h
@@ -1,10 +1,10 @@
-#import <UIKit/UIKit.h>
-#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
 #import <React/RCTBridgeDelegate.h>
+#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
+#import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
-@property(nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
-@property(nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
+@property (nonatomic, strong) UIWindow *window;
 
 @end
