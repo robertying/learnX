3ceae20353cd31cf358e9e6ff78e626a3ddf1970
Robert Ying <yingrui205@gmail.com>
2019-06-29 00:29:21 +0800

    Prune dependencies & use latest autolink feature

--- scripts/pack_secrets.sh
+++ scripts/pack_secrets.sh
@@ -4,6 +4,6 @@ BASEDIR=$(dirname "$0")
 cd $BASEDIR/../
 
 tar cvf secrets.tar.gz android/keystores/release.keystore fastlane/key.json \
-    ios/GoogleService-Info.plist ios/CodePush.plist \
-    android/app/google-services.json android/app/src/main/res/values/codepush.xml
+    ios/GoogleService-Info.plist \
+    android/app/google-services.json
 travis encrypt-file --com secrets.tar.gz --add
