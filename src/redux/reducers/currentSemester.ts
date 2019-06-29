ea0cb92e758fca4cd53a5bb1ba2975a21119c4a7
Rui Ying <yingrui205@gmail.com>
2019-06-30 02:25:04 +0800

    Set default semester to `2019-2020-1`

--- src/redux/reducers/currentSemester.ts
+++ src/redux/reducers/currentSemester.ts
@@ -2,7 +2,7 @@ import { ISetCurrentSemesterAction } from "../types/actions";
 import { SET_CURRENT_SEMESTER } from "../types/constants";
 
 export default function currentSemester(
-  state: string = "2018-2019-2",
+  state: string = "2019-2020-1",
   action: ISetCurrentSemesterAction
 ): string {
   switch (action.type) {
