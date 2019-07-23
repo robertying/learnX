568109edd9d8d0fb3641e1f04993bf22bc94aaea
Rui Ying <yingrui205@gmail.com>
2019-07-22 17:46:08 -0700

    Add splash screen for Android

--- android/app/src/main/java/io/robertying/learnx/MainActivity.java
+++ android/app/src/main/java/io/robertying/learnx/MainActivity.java
@@ -1,7 +1,14 @@
 package io.robertying.learnx;
 
+import android.view.View;
+
 import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
-
+    @Override
+    protected void addDefaultSplashLayout() {
+        View view = new View(this);
+        view.setBackground(getDrawable(R.drawable.splash_screen));
+        setContentView(view);
+    }
 }
