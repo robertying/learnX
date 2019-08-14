a6ed4ad3f9a0c202e764355b77d16e5ba06082e7
Rui Ying <yingrui205@gmail.com>
2019-08-14 15:23:18 -0700

    Use `patch-package` for patches

--- scripts/fix_react_native_interactable.sh
+++ scripts/fix_react_native_interactable.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cp -r patches/node_modules .
-
 pushd node_modules/react-native-interactable
 rm -rf \{ios\,android\} && rm -rf android && rm -rf ios
 ln -s lib/android . && ln -s lib/ios .
