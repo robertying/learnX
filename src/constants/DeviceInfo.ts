65b4a20575ee34a4a6fc921ca857153d04ae9b0b
Rui Ying <yingrui205@gmail.com>
2019-09-24 20:55:16 +0800

    Add build number to AboutScreen

--- src/constants/DeviceInfo.ts
+++ src/constants/DeviceInfo.ts
@@ -2,14 +2,16 @@ import {Platform} from 'react-native';
 import Info from 'react-native-device-info';
 
 let _isIPad: boolean;
+let _buildNo: string;
 
 const init = async () => {
   _isIPad = (await Info.isTablet()) && Platform.OS === 'ios';
+  _buildNo = await Info.getBuildNumber();
 };
 
 const isIPad = () => _isIPad;
 
 export default {
   init,
-  isIPad,
+  buildNo: () => _buildNo,
 };
