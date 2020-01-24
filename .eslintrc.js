d22bd61ac960e1463a3d118e83ba8f27aed07773
Rui Ying <yingrui205@gmail.com>
2020-01-24 21:17:32 +0800

    Revert eslint config change

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
   root: true,
+  env: {
+    jest: true,
+    jasmine: true,
+  },
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    'no-shadow': 'off',
+  },
 };
