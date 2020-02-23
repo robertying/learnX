5de4411dd6093c883fe83f842295d885995697f6
Rui Ying <yingrui205@gmail.com>
2020-02-23 19:08:25 +0800

    Remove flipper

--- android/app/src/main/java/io/robertying/learnx/MainApplication.java
+++ android/app/src/main/java/io/robertying/learnx/MainApplication.java
@@ -2,16 +2,10 @@ package io.robertying.learnx;
 
 import com.facebook.react.PackageList;
 
-import android.content.Context;
-
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.soloader.SoLoader;
-
-import java.lang.reflect.InvocationTargetException;
 
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
 import org.unimodules.adapters.react.ReactModuleRegistryProvider;
@@ -55,41 +49,4 @@ public class MainApplication extends NavigationApplication {
         return mReactNativeHost;
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
-        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    }
-
-    /**
-     * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-     * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-     *
-     * @param context
-     * @param reactInstanceManager
-     */
-    private static void initializeFlipper(
-            Context context, ReactInstanceManager reactInstanceManager) {
-        if (BuildConfig.DEBUG) {
-            try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-                Class<?> aClass = Class.forName("io.robertying.learnx.ReactNativeFlipper");
-                aClass
-                        .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-                        .invoke(null, context, reactInstanceManager);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
