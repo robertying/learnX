25792bea8dd3a9501057734a584b79e40a57b595
Rui Ying <yingrui205@gmail.com>
2020-08-09 06:43:11 +0800

    v11.0.0

--- scripts/pack_secrets.sh
+++ scripts/pack_secrets.sh
@@ -5,5 +5,6 @@ cd $BASEDIR/../
 
 tar cvf secrets.tar.gz android/keystores/release.keystore fastlane/key.json \
     ios/GoogleService-Info.plist \
-    android/app/google-services.json
+    android/app/google-services.json \
+    .env
 travis encrypt-file --com secrets.tar.gz --add
