8e9ec3549fdc3c3fe01f86bdb067320818713ff2
Rui Ying <yingrui205@gmail.com>
2021-03-26 17:25:37 +0800

    Add navigation toolbar to courseX webview

--- src/screens/CourseX.tsx
+++ src/screens/CourseX.tsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
-import {ProgressBar} from 'react-native-paper';
-import WebView from 'react-native-webview';
+import React, {useRef, useState} from 'react';
+import {Linking, StyleSheet, View} from 'react-native';
+import {IconButton, ProgressBar, useTheme} from 'react-native-paper';
+import WebView, {WebViewNavigation} from 'react-native-webview';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {StackScreenProps} from '@react-navigation/stack';
 import SafeArea from 'components/SafeArea';
 import {ScreenParams} from './types';
@@ -11,12 +12,26 @@ const CourseX: React.FC<StackScreenProps<ScreenParams, 'CourseX'>> = ({
 }) => {
   const courseId = route.params?.id ?? '';
 
+  const theme = useTheme();
+  const safeAreaInsets = useSafeAreaInsets();
+
+  const webViewRef = useRef<WebView | null>(null);
   const [progress, setProgress] = useState(0);
+  const [currentUrl, setCurrentUrl] = useState('');
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
+
+  const handleNavigation = (e: WebViewNavigation) => {
+    setCurrentUrl(e.url);
+    setCanGoBack(e.canGoBack);
+    setCanGoForward(e.canGoForward);
+  };
 
   return (
     <SafeArea>
       {progress && progress !== 1 ? <ProgressBar progress={progress} /> : null}
       <WebView
+        ref={webViewRef}
         style={styles.webview}
         source={{
           uri:
@@ -28,7 +43,37 @@ const CourseX: React.FC<StackScreenProps<ScreenParams, 'CourseX'>> = ({
         onLoadProgress={({nativeEvent}) => {
           setProgress(nativeEvent.progress);
         }}
+        onNavigationStateChange={handleNavigation}
       />
+      <View
+        style={[
+          styles.toolbar,
+          {
+            backgroundColor: theme.colors.surface,
+            paddingBottom: safeAreaInsets.bottom,
+          },
+        ]}>
+        <IconButton
+          icon="arrow-left"
+          disabled={!canGoBack}
+          onPress={() => webViewRef.current?.goBack()}
+        />
+        <IconButton
+          icon="arrow-right"
+          disabled={!canGoForward}
+          onPress={() => webViewRef.current?.goForward()}
+        />
+        <IconButton
+          icon="refresh"
+          disabled={progress && progress !== 1 ? true : false}
+          onPress={() => webViewRef.current?.reload()}
+        />
+        <IconButton
+          icon="open-in-new"
+          disabled={progress && progress !== 1 ? true : false}
+          onPress={() => Linking.openURL(currentUrl)}
+        />
+      </View>
     </SafeArea>
   );
 };
@@ -36,6 +81,11 @@ const CourseX: React.FC<StackScreenProps<ScreenParams, 'CourseX'>> = ({
 const styles = StyleSheet.create({
   webview: {
     backgroundColor: 'transparent',
+  },
+  toolbar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingHorizontal: 16,
   },
 });
 
