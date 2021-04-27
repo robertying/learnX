51d5ab2609e712a99d0aed7f1b0ea4917661f670
Rui Ying <yingrui205@gmail.com>
2021-04-27 22:58:05 +0800

    Fix catalyst detection on 10.15

--- src/constants/DeviceInfo.ts
+++ src/constants/DeviceInfo.ts
@@ -4,6 +4,9 @@ export default {
   buildNo: () => Info.getBuildNumber(),
   isTablet: () => Info.isTablet(),
   isMac: () =>
-    Info.getSystemName() === 'iOS' && Info.getDeviceType() === 'Desktop',
+    (Info.getSystemName() === 'iOS' ||
+      Info.getSystemName() === 'iPhone OS' ||
+      Info.getSystemName() === 'Mac OS X') &&
+    Info.getDeviceType() === 'Desktop',
   abi: async () => (await Info.supportedAbis())?.[0] || 'universal',
 };
