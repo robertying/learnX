1f7c39c1b555b1ea892cc531c7fcbc4936a17994
Rui Ying <yingrui205@gmail.com>
2021-11-07 03:19:29 -0800

    Fix mac detection on monterey

--- src/constants/DeviceInfo.ts
+++ src/constants/DeviceInfo.ts
@@ -4,8 +4,9 @@ export default {
   buildNo: () => Info.getBuildNumber(),
   isTablet: () => Info.isTablet(),
   isMac: () =>
-    (Info.getSystemName() === 'iOS' ||
-      Info.getSystemName() === 'iPhone OS' ||
+    (Info.getSystemName() === 'iPhone OS' ||
+      Info.getSystemName() === 'iOS' ||
+      Info.getSystemName() === 'iPadOS' ||
       Info.getSystemName() === 'Mac OS X') &&
     Info.getDeviceType() === 'Desktop',
   abi: async () => (await Info.supportedAbis())?.[0] || 'universal',
