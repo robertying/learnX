686f765f915b5afb111b98ed2659814e20318790
Rui Ying <yingrui205@gmail.com>
2022-01-21 16:05:43 -0800

    Sign the test bundle

--- scripts/test_android_bundle.sh
+++ scripts/test_android_bundle.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 APK_DIR="android/app/build/outputs/bundle/release"
+KS_DIR="android/app/release.keystore"
 
-bundletool build-apks --bundle=$APK_DIR/app-release.aab --output=$APK_DIR/app-release.apks --overwrite
+bundletool build-apks --bundle=$APK_DIR/app-release.aab --output=$APK_DIR/app-release.apks --ks=$KS_DIR --ks-key-alias=io.robertying.learnx.key --overwrite
 bundletool install-apks --apks=$APK_DIR/app-release.apks
