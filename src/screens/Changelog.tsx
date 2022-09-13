1e8adf67cbced686c964e119b63d48a12ad5a652
Rui Ying <git@ruiying.io>
2022-09-13 04:27:14 -0700

    Add loading progress for Changelog

--- src/screens/Changelog.tsx
+++ src/screens/Changelog.tsx
@@ -1,13 +1,14 @@
-import {useEffect, useRef} from 'react';
-import {Linking} from 'react-native';
+import {useEffect, useRef, useState} from 'react';
+import {Linking, StyleSheet} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import WebView, {WebViewNavigation} from 'react-native-webview';
+import {ProgressBar} from 'react-native-paper';
 import SafeArea from 'components/SafeArea';
+import {useAppDispatch} from 'data/store';
 import {setSetting} from 'data/actions/settings';
 import useNavigationAnimation from 'hooks/useNavigationAnimation';
 import {ScreenParams} from './types';
 import packageJson from '../../package.json';
-import {useAppDispatch} from 'data/store';
 
 const Changelog: React.FC<
   React.PropsWithChildren<NativeStackScreenProps<ScreenParams, 'Changelog'>>
@@ -15,6 +16,7 @@ const Changelog: React.FC<
   const dispatch = useAppDispatch();
 
   const webViewRef = useRef<WebView>(null);
+  const [progress, setProgress] = useState(0);
 
   const onNavigationStateChange = (e: WebViewNavigation) => {
     if (e.navigationType === 'click') {
@@ -33,10 +35,14 @@ const Changelog: React.FC<
 
   return (
     <SafeArea>
+      {progress ? (
+        <ProgressBar style={styles.progressBar} progress={progress} />
+      ) : undefined}
       <WebView
         ref={webViewRef}
         originWhitelist={['https://github.com']}
         onNavigationStateChange={onNavigationStateChange}
+        onLoadProgress={({nativeEvent}) => setProgress(nativeEvent.progress)}
         decelerationRate="normal"
         androidHardwareAccelerationDisabled={true}
         source={{
@@ -47,5 +53,15 @@ const Changelog: React.FC<
     </SafeArea>
   );
 };
+
+const styles = StyleSheet.create({
+  progressBar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 99,
+  },
+});
 
 export default Changelog;
