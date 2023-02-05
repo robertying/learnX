392acc2cdbe2ab121ccb2eb5e9292598ecff50e9
Rui Ying <git@ruiying.io>
2023-02-04 16:56:52 -0800

    Add a script to rebuild react native to apply my utf-8 fix

---
+++ scripts/rebuild_react_native_with_utf8_fix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install node
+npm install -g yarn
+
+git clone https://github.com/facebook/react-native
+cd react-native
+git checkout v0.71.2
+
+curl -fsSL https://github.com/robertying/react-native-fix/commit/1624343e702e31946e2c33dba2d3c123872df94e.patch -o utf8-fix.patch
+git apply utf8-fix.patch
+
+yarn install
+docker run --rm --name rn-build -v $PWD:/pwd -w /pwd reactnativecommunity/react-native-android /bin/sh -c "./gradlew installArchives"
