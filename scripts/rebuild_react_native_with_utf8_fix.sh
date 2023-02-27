04f77af19048ffe809369313bb8d15f9868c166b
Rui Ying <git@ruiying.io>
2023-02-26 16:03:15 -0800

    Upgrade dependencies

--- scripts/rebuild_react_native_with_utf8_fix.sh
+++ scripts/rebuild_react_native_with_utf8_fix.sh
@@ -11,7 +11,7 @@ npm install -g yarn
 
 git clone https://github.com/facebook/react-native
 cd react-native
-git checkout v0.71.2
+git checkout v0.71.3
 
 curl -fsSL https://github.com/robertying/react-native-fix/commit/1624343e702e31946e2c33dba2d3c123872df94e.patch -o utf8-fix.patch
 git apply utf8-fix.patch
