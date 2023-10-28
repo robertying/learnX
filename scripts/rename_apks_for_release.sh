108abfc718e42dcc98cea4d16c9593d390912917
Rui Ying <git@ruiying.io>
2023-10-28 02:04:22 -0700

    Update rename script

--- scripts/rename_apks_for_release.sh
+++ scripts/rename_apks_for_release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-APK_DIR="android/app/build/outputs/apk/release"
+DIR="$(dirname "$0")"
+APK_DIR="${DIR}/../android/app/build/outputs/apk/release"
 cp $APK_DIR/app-x86_64-release.apk $APK_DIR/learnX-x86_64-$VERSION_TAG.apk
 cp $APK_DIR/app-arm64-v8a-release.apk $APK_DIR/learnX-arm64-v8a-$VERSION_TAG.apk
 cp $APK_DIR/app-armeabi-v7a-release.apk $APK_DIR/learnX-armeabi-v7a-$VERSION_TAG.apk
