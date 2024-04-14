5c2b2154c4c74e028d3525e1ce2147329cc98f8c
Rui Ying <git@ruiying.io>
2024-04-14 05:02:49 -0700

    Add Buffer polyfill

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+import './polyfills';
+
 import 'react-native-gesture-handler';
-import 'react-native-url-polyfill/auto';
-import {AppRegistry, Platform, UIManager} from 'react-native';
+
+import {AppRegistry, LogBox, Platform, UIManager} from 'react-native';
 import {name as appName} from './app.json';
 import App from './src/App';
 import './src/helpers/notification';
 import './src/helpers/background';
+
+LogBox.ignoreAllLogs(true);
 
 if (
   Platform.OS === 'android' &&
---
+++ polyfills.js
@@ -0,0 +1,4 @@
+import {Buffer} from 'buffer';
+global.Buffer = Buffer;
+
+import 'react-native-url-polyfill/auto';
