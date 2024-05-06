6bcd6c72ad2a60b1559c280b5f926777847c2f80
Rui Ying <git@ruiying.io>
2024-05-05 17:32:43 -0700

    Set language before fetching user info

--- src/data/actions/user.ts
+++ src/data/actions/user.ts
@@ -1,5 +1,5 @@
 import {createAsyncAction} from 'typesafe-actions';
-import {CourseType} from 'thu-learn-lib';
+import {CourseType, Language} from 'thu-learn-lib';
 import {
   GET_USER_INFO_FAILURE,
   GET_USER_INFO_REQUEST,
@@ -8,6 +8,7 @@ import {
 import {User} from 'data/types/state';
 import {ThunkResult} from 'data/types/actions';
 import {dataSource} from 'data/source';
+import {isLocaleChinese} from 'helpers/i18n';
 
 export const getUserInfoAction = createAsyncAction(
   GET_USER_INFO_REQUEST,
@@ -18,6 +19,12 @@ export const getUserInfoAction = createAsyncAction(
 export function getUserInfo(): ThunkResult {
   return async dispatch => {
     dispatch(getUserInfoAction.request());
+
+    const lang = isLocaleChinese() ? Language.ZH : Language.EN;
+
+    try {
+      await dataSource.setLanguage(lang);
+    } catch (err) {}
 
     try {
       const userInfo = await dataSource.getUserInfo(CourseType.STUDENT);
