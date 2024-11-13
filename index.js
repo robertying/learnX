b49cdaf51e85db71923ac0887aa2ed7bf09a76f1
Rui Ying <git@ruiying.io>
2024-11-13 13:55:55 -0800

    Disable reanimated warning logs

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import './polyfills';
 
 import {AppRegistry, LogBox, Platform, UIManager} from 'react-native';
+import {
+  configureReanimatedLogger,
+  ReanimatedLogLevel,
+} from 'react-native-reanimated';
 import {name as appName} from './app.json';
 import App from './src/App';
+
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false,
+});
 
 LogBox.ignoreAllLogs(true);
 
