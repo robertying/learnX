352df098c296430ff1f661c816db28d82bd3efb5
Rui Ying <git@ruiying.io>
2025-04-10 10:49:39 -0700

    Update SafeArea implementation

--- src/components/SafeArea.tsx
+++ src/components/SafeArea.tsx
@@ -1,11 +1,25 @@
-import {SafeAreaView, SafeAreaViewProps} from 'react-native-safe-area-context';
+import {View} from 'react-native';
+import {
+  SafeAreaViewProps,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 import Styles from 'constants/Styles';
 
 const SafeArea: React.FC<
   React.PropsWithChildren<SafeAreaViewProps>
 > = props => {
+  const insets = useSafeAreaInsets();
   return (
-    <SafeAreaView style={Styles.flex1} edges={['left', 'right']} {...props} />
+    <View
+      style={[
+        Styles.flex1,
+        {
+          paddingLeft: insets.left,
+          paddingRight: insets.right,
+        },
+      ]}
+      {...props}
+    />
   );
 };
 
