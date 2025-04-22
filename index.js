fb625f7e8ed2a9f52e086a305d66b442962935ff
Rui Ying <git@ruiying.io>
2025-04-21 21:19:59 -0700

    Add back reanimated logs

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 import './polyfills';
 
 import {AppRegistry, LogBox, Platform, UIManager} from 'react-native';
-import {
-  configureReanimatedLogger,
-  ReanimatedLogLevel,
-} from 'react-native-reanimated';
 import {name as appName} from './app.json';
 import App from './src/App';
-
-configureReanimatedLogger({
-  level: ReanimatedLogLevel.warn,
-  strict: false,
-});
 
 LogBox.ignoreAllLogs(true);
 
