9689dd83ecd09e2482b76a47d8650374ea7df390
Rui Ying <git@ruiying.io>
2025-08-25 19:48:41 -0700

    Enable new arch on Android

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 import './polyfills';
 
-import {AppRegistry, LogBox, Platform, UIManager} from 'react-native';
-import {name as appName} from './app.json';
+import { AppRegistry, LogBox } from 'react-native';
+import { name as appName } from './app.json';
 import App from './src/App';
 
 LogBox.ignoreAllLogs(true);
-
-if (
-  Platform.OS === 'android' &&
-  UIManager.setLayoutAnimationEnabledExperimental
-) {
-  UIManager.setLayoutAnimationEnabledExperimental(true);
-}
 
 AppRegistry.registerComponent(appName, () => App);
