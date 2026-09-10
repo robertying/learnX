a684d5f336adde61fdb9ae5321dbbe8269a395ff
Rui Ying <git@ruiying.io>
2026-09-10 14:50:36 -0700

    Polyfill crypto module

--- polyfills.js
+++ polyfills.js
@@ -1,5 +1,18 @@
 import { Buffer } from 'buffer';
 global.Buffer = Buffer;
 
+import * as ExpoCrypto from 'expo-crypto';
+global.crypto = {
+  getRandomValues: ExpoCrypto.getRandomValues,
+  randomUUID: ExpoCrypto.randomUUID,
+  subtle: {
+    digest: (algorithm, data) =>
+      ExpoCrypto.digest(
+        typeof algorithm === 'string' ? algorithm : algorithm.name,
+        data,
+      ),
+  },
+};
+
 import 'react-native-url-polyfill/auto';
 import 'react-native-gesture-handler';
